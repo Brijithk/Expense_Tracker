4dc19d0d7c090ff85d338db66c9f73b768010b95
Brijithk <keranbrijith@gmail.com>
2026-02-14 05:12:16 +0530

    added mysql connection

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'expense_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'expense_db',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
